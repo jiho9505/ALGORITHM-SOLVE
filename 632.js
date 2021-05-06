3f73461ee42a9c0658614a84ce6f8cbae3fa18d7
jiho9505 <sun_days@naver.com>
2021-05-06 13:24:41 +0900

    leetcode_Top100

--- 632.js
+++ 632.js
@@ -1,3 +1,8 @@
+/*
+    어려운 투포인터!
+    object에 음수가 들어가면 양수보다 뒤에 나온다 key 값이...
+*/
+
 var smallestRange = function(nums) {
     let d = {};
     let arr = [];
@@ -14,39 +19,39 @@ var smallestRange = function(nums) {
         arr.push(parseInt(key));
     }
     arr.sort((a,b) => a-b);
-    console.log('arr',arr)
+
     let end = 0;
     let start = 0
-    // for(let start = 0; start<arr.length; start++){
-        while(end < arr.length){
-            let find = false;
-            for(let i = 0; i< nums.length; i++){
-                find = false;
-                for(let j = 0; j< nums[i].length; j++){
-                    if(arr[start] <= nums[i][j] && nums[i][j] <=arr[end]){
-                        find = true;
-                        break;
-                    }
-                }
-                if(!find){
-                    end++;
-                    console.log('e',end)
+
+    while(end < arr.length){
+        let find = false;
+        for(let i = 0; i< nums.length; i++){
+            find = false;
+            for(let j = 0; j< nums[i].length; j++){
+                if(arr[start] <= nums[i][j] && nums[i][j] <=arr[end]){
+                    find = true;
                     break;
                 }
             }
-            if(find){
-                if(start === end) return [arr[start],arr[end]];
-                else{
-                    if(arr[end] - arr[start] < min){
-                        answer = [arr[start],arr[end]];
-                        min = arr[end] - arr[start];
-                    }
-                    start++;
-                    console.log('s',start)
-                }
+            if(!find){
+                end++;
+                console.log('e',end)
+                break;
             }
         }
-    // }
+        if(find){
+            if(start === end) return [arr[start],arr[end]];
+            else{
+                if(arr[end] - arr[start] < min){
+                    answer = [arr[start],arr[end]];
+                    min = arr[end] - arr[start];
+                }
+                start++;
+    
+            }
+        }
+    }
+    
     
     return answer;
 };
--- etc.js
+++ etc.js
@@ -2,5 +2,9 @@
 -좋은 문제 선별-
 
 124. Binary Tree Maximum Path Sum
-=> 그래프 BFS
+// 그래프 BFS
+
+895. Maximum Frequency Stack
+// hash
+
 */
